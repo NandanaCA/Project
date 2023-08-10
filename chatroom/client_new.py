adb017d1cfc577f05637f20a62bbce380d154163
NandanaCA <141863296+NandanaCA@users.noreply.github.com>
2023-08-10 12:25:33 +0530

    Update client_new.py

--- chatroom/client_new.py
+++ chatroom/client_new.py
@@ -18,10 +18,11 @@ from tkinter import ttk
 root_window=Tk()
 root_window.withdraw()
 
-enter_button=PhotoImage(file='./icons/enter100_32.png')
-login_button=PhotoImage(file='./icons/login150_48.png')
-search_button=PhotoImage(file='./icons/search100_32.png')
-history_button=PhotoImage(file='./icons/history100_32.png')
+enter_button=PhotoImage(file='C:/Users/nanda/OneDrive/Desktop/COMPUTER_PROJECT/ICONS/enter100_32.png')
+login_button=PhotoImage(file='C:/Users/nanda/OneDrive/Desktop/COMPUTER_PROJECT/ICONS/login150_48.png')
+search_button=PhotoImage(file='C:/Users/nanda/OneDrive/Desktop/COMPUTER_PROJECT/ICONS/search100_32.png')
+history_button=PhotoImage(file='C:/Users/nanda/OneDrive/Desktop/COMPUTER_PROJECT/ICONS/history100_32.png')
+    
 
 
 def send_message(choice,text=''):
@@ -29,7 +30,7 @@ def send_message(choice,text=''):
     print(text)
     client.sendall(text.encode())
                     
-def listen1(chat_tree):
+def listen1():
     while True:
         message=client.recv(2048).decode('utf-8')
         choice=message[0]
@@ -40,7 +41,9 @@ def listen1(chat_tree):
             username=split_message[0][1:]
             content=split_message[1]
             date=split_message[2]
-            chat_tree.insert('','end',text=1,values=(username,content,date))
+##            label=Label(mwframe2,text=content,font=('Britannic Bold','26','bold'))
+##            label.pack()
+            screen.insert('','end',text=1,values=(username,content,date))
             print(date,' ',username,':',content)
             
         elif choice=='2':
@@ -68,6 +71,7 @@ def listen1(chat_tree):
 
             style.map('Treeview',background=[('selected','#083447')])
 
+
             chat_frame2=LabelFrame(chat_frame)
             chat_frame2.pack(fill='both',expand='yes')
             chat_frame3=Frame(chat_frame2)
@@ -76,35 +80,35 @@ def listen1(chat_tree):
             chat_scrollbar=Scrollbar(chat_frame3)
             chat_scrollbar.pack(side=RIGHT,fill=Y)
 
-            chat_tree=ttk.Treeview(chat_frame3,yscrollcommand=chat_scrollbar.set)
-            chat_tree.pack(fill=BOTH,expand=YES)
 
-            chat_scrollbar.config(command=chat_tree.yview)
+            screen2=ttk.Treeview(chat_frame3,yscrollcommand=chat_scrollbar.set)
+            screen2.pack(fill=BOTH,expand=YES)
 
-            chat_tree['columns']=("Participants","message","time")
-            chat_tree.column("#0",width=0)
-            chat_tree.column("Participants",anchor=W,width=150,minwidth=150)
-            chat_tree.column("message",anchor=W,width=150,minwidth=150)
-            chat_tree.column("time",anchor=W,width=150,minwidth=150)
+            chat_scrollbar.config(command=screen2.yview)
 
-
-
-            chat_tree.heading("#0",text="",anchor=W)
-            chat_tree.heading("Participants",text="PARTICIPANTS",anchor=W)
-            chat_tree.heading("message",text="MESSAGE",anchor=W)
-            chat_tree.heading("time",text="TIME",anchor=W)
+            screen2['columns']=("Participants","message","time")
+            screen2.column("#0",width=0)
+            screen2.column("Participants",anchor=W,width=150,minwidth=150)
+            screen2.column("message",anchor=W,width=150,minwidth=150)
+            screen2.column("time",anchor=W,width=150,minwidth=150)
 
 
 
-            chat_tree.tag_configure('oddrow',background="white")
-            chat_tree.tag_configure('evenrow',background="#99bccf")
+            screen2.heading("#0",text="",anchor=W)
+            screen2.heading("Participants",text="PARTICIPANTS",anchor=W)
+            screen2.heading("message",text="MESSAGE",anchor=W)
+            screen2.heading("time",text="TIME",anchor=W)
 
-            chat_tree.insert(parent='',index='end',iid=0,text="",values=(""),tags=('evenrow',))
+
+
+            screen2.tag_configure('oddrow',background="white")
+            screen2.tag_configure('evenrow',background="#99bccf")
+
+            screen2.insert(parent='',index='end',iid=0,text="",values=(""),tags=('evenrow',))
 
             chat_frame4=LabelFrame(chat_frame,foreground='#083447')
             chat_frame4.pack(fill="x")
 
-            Label(chat_frame4,text="").pack()
 
             footer_frame=Frame(main_window,background='#083447')
             footer_frame.pack(fill=X)
@@ -116,13 +120,7 @@ def listen1(chat_tree):
             history=eval(message)
             for i in history:
                 print(i)
-                chat_tree.insert('','end',text=1,values=(i[1],i[2],i[3]))
-
-            message=message[1:]
-            history=eval(message)
-            for i in history:
-                print(i)
-                chat_tree.insert('','end',text=1,values=(i[1],i[2],i[3]))
+                screen2.insert('','end',text=1,values=(i[1],i[2],i[3]))
                 
         elif choice=='3':
             main_window=Toplevel()
@@ -149,6 +147,7 @@ def listen1(chat_tree):
 
             style.map('Treeview',background=[('selected','#083447')])
 
+
             chat_frame2=LabelFrame(chat_frame)
             chat_frame2.pack(fill='both',expand='yes')
             chat_frame3=Frame(chat_frame2)
@@ -157,47 +156,46 @@ def listen1(chat_tree):
             chat_scrollbar=Scrollbar(chat_frame3)
             chat_scrollbar.pack(side=RIGHT,fill=Y)
 
-            chat_tree=ttk.Treeview(chat_frame3,yscrollcommand=chat_scrollbar.set)
-            chat_tree.pack(fill=BOTH,expand=YES)
 
-            chat_scrollbar.config(command=chat_tree.yview)
+            screen3=ttk.Treeview(chat_frame3,yscrollcommand=chat_scrollbar.set)
+            screen3.pack(fill=BOTH,expand=YES)
 
-            chat_tree['columns']=("Participants","message","time")
-            chat_tree.column("#0",width=0)
-            chat_tree.column("Participants",anchor=W,width=150,minwidth=150)
-            chat_tree.column("message",anchor=W,width=150,minwidth=150)
-            chat_tree.column("time",anchor=W,width=150,minwidth=150)
+            chat_scrollbar.config(command=screen3.yview)
 
-
-
-            chat_tree.heading("#0",text="",anchor=W)
-            chat_tree.heading("Participants",text="PARTICIPANTS",anchor=W)
-            chat_tree.heading("message",text="MESSAGE",anchor=W)
-            chat_tree.heading("time",text="TIME",anchor=W)
+            screen3['columns']=("Participants","message","time")
+            screen3.column("#0",width=0)
+            screen3.column("Participants",anchor=W,width=150,minwidth=150)
+            screen3.column("message",anchor=W,width=150,minwidth=150)
+            screen3.column("time",anchor=W,width=150,minwidth=150)
 
 
 
-            chat_tree.tag_configure('oddrow',background="white")
-            chat_tree.tag_configure('evenrow',background="#99bccf")
+            screen3.heading("#0",text="",anchor=W)
+            screen3.heading("Participants",text="PARTICIPANTS",anchor=W)
+            screen3.heading("message",text="MESSAGE",anchor=W)
+            screen3.heading("time",text="TIME",anchor=W)
 
-            chat_tree.insert(parent='',index='end',iid=0,text="",values=(""),tags=('evenrow',))
+
+
+            screen3.tag_configure('oddrow',background="white")
+            screen3.tag_configure('evenrow',background="#99bccf")
+
+            screen3.insert(parent='',index='end',iid=0,text="",values=(""),tags=('evenrow',))
 
             chat_frame4=LabelFrame(chat_frame,foreground='#083447')
             chat_frame4.pack(fill="x")
 
-            Label(chat_frame4,text="").pack()
 
             footer_frame=Frame(main_window,background='#083447')
             footer_frame.pack(fill=X)
             Label(footer_frame,text="",background='#083447').pack()
 
 
-
             message=message[1:]
             history=eval(message)
             for i in history:
                 print(i)
-                chat_tree.insert('','end',text=1,values=(i[1],i[2],i[3]))
+                screen3.insert('','end',text=1,values=(i[1],i[2],i[3]))
 
     
 
@@ -220,8 +218,7 @@ def main():
         text=textbox1.get()
         textbox1.delete(0,END)
         send_message(choice,text)
-
-
+        
     main_window=Toplevel()
     main_window.title("chat")
     main_window.geometry("400x600")
@@ -229,18 +226,18 @@ def main():
     main_nb=ttk.Notebook(main_window)
     main_nb.pack(fill=BOTH,expand=1)
 
-    chat_frame=Frame(main_nb)
+    chat_framemw=Frame(main_nb)
 
-    main_nb.add(chat_frame,text="chat room")
+    main_nb.add(chat_framemw,text="chat room")
 
-    chat_frame1=Frame(chat_frame,background='#083447')
-    chat_frame1.pack(fill=X)
+    chat_framemw1=Frame(chat_framemw,background='#083447')
+    chat_framemw1.pack(fill=X)
 
-    Label(chat_frame1,text="",background='#083447').pack()
-    Label(chat_frame1,text="",background='#083447').pack()
-    b1=Button(chat_frame1,image=history_button,borderwidth=0,command=history)
+    Label(chat_framemw1,text="",background='#083447').pack()
+    Label(chat_framemw1,text="",background='#083447').pack()
+    b1=Button(chat_framemw1,image=history_button,borderwidth=0,command=history)
     b1.place(x=10,y=6)
-    b2=Button(chat_frame1,image=search_button,borderwidth=0,command=search)
+    b2=Button(chat_framemw1,image=search_button,borderwidth=0,command=search)
     b2.place(x=280,y=6)
 
 
@@ -251,50 +248,51 @@ def main():
 
     style.map('Treeview',background=[('selected','#083447')])
 
-    chat_frame2=LabelFrame(chat_frame)
-    chat_frame2.pack(fill='both',expand='yes')
-    chat_frame3=Frame(chat_frame2)
-    chat_frame3.pack(fill=BOTH,expand='yes')
+    global chat_framemw2
+    chat_framemw2=LabelFrame(chat_framemw)
+    chat_framemw2.pack(fill='both',expand='yes')
+    chat_framemw3=Frame(chat_framemw2)
+    chat_framemw3.pack(fill=BOTH,expand='yes')
 
-    chat_scrollbar=Scrollbar(chat_frame3)
+    chat_scrollbar=Scrollbar(chat_framemw3)
     chat_scrollbar.pack(side=RIGHT,fill=Y)
-    
-    #global chat_tree
-    chat_tree=ttk.Treeview(chat_frame3,yscrollcommand=chat_scrollbar.set)
-    chat_tree.pack(fill=BOTH,expand=YES)
 
-    chat_scrollbar.config(command=chat_tree.yview)
+    global screen
+    screen=ttk.Treeview(chat_framemw3,yscrollcommand=chat_scrollbar.set)
+    screen.pack(fill=BOTH,expand=YES)
 
-    chat_tree['columns']=("Participants","message","time")
-    chat_tree.column("#0",width=0)
-    chat_tree.column("Participants",anchor=W,width=150,minwidth=150)
-    chat_tree.column("message",anchor=W,width=150,minwidth=150)
-    chat_tree.column("time",anchor=W,width=150,minwidth=150)
+    chat_scrollbar.config(command=screen.yview)
 
-
-
-    chat_tree.heading("#0",text="",anchor=W)
-    chat_tree.heading("Participants",text="PARTICIPANTS",anchor=W)
-    chat_tree.heading("message",text="MESSAGE",anchor=W)
-    chat_tree.heading("time",text="TIME",anchor=W)
+    screen['columns']=("Participants","message","time")
+    screen.column("#0",width=0)
+    screen.column("Participants",anchor=W,width=150,minwidth=150)
+    screen.column("message",anchor=W,width=150,minwidth=150)
+    screen.column("time",anchor=W,width=150,minwidth=150)
 
 
 
-    chat_tree.tag_configure('oddrow',background="white")
-    chat_tree.tag_configure('evenrow',background="#99bccf")
+    screen.heading("#0",text="",anchor=W)
+    screen.heading("Participants",text="PARTICIPANTS",anchor=W)
+    screen.heading("message",text="MESSAGE",anchor=W)
+    screen.heading("time",text="TIME",anchor=W)
 
-    chat_tree.insert(parent='',index='end',iid=0,text="",values=(""),tags=('evenrow',))
 
-    chat_frame4=LabelFrame(chat_frame,foreground='#083447')
-    chat_frame4.pack(fill="x")
 
-    textbox1=Entry(chat_frame4,width=80,font="none 8")
+    screen.tag_configure('oddrow',background="white")
+    screen.tag_configure('evenrow',background="#99bccf")
+
+    screen.insert(parent='',index='end',iid=0,text="",values=(""),tags=('evenrow',))
+
+    chat_framemw4=LabelFrame(chat_framemw,foreground='#083447')
+    chat_framemw4.pack(fill="x")
+
+    textbox1=Entry(chat_framemw4,width=80,font="none 8")
     textbox1.pack()
 
-    threading.Thread(target=listen1,args=(chat_tree,)).start()
+    threading.Thread(target=listen1).start()
 
-    Label(chat_frame4,text="").pack()
-    b1=Button(chat_frame4,image=enter_button,borderwidth=0,command=send)
+    Label(chat_framemw4,text="").pack()
+    b1=Button(chat_framemw4,image=enter_button,borderwidth=0,command=send)
     b1.pack()
 
 
